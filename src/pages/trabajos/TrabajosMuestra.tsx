5a451d76ca10e8ec1244a14c6fd320fb71a3ccab
romartiguillem-spec <romarti.guillem@gmail.com>
2026-04-19 17:03:27 +0200

    Remate total

--- src/pages/trabajos/TrabajosMuestra.tsx
+++ src/pages/trabajos/TrabajosMuestra.tsx
@@ -11,7 +11,7 @@ export const TrabajosMuestra = () => {
   return (
     <div id="trabajos" className="bg-slate-950">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-white">
+        <h2 className="text-2xl font-bold tracking-tight text-white mt-10">
           Trabajos realizados
         </h2>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
